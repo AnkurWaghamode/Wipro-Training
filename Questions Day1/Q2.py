a2ca393b3344eecbd10213b77b8bdde74fed78b4
AnkurWaghamode <168619721+AnkurWaghamode@users.noreply.github.com>
2026-01-13 16:59:51 +0530

    Add list comprehension and set/dictionary examples

--- Questions Day1/Q2.py
+++ Questions Day1/Q2.py
@@ -1,1 +1,10 @@
+data = [1, 2, 3, 4, 5, 6, 2, 4]
 
+squares = [x**2 for x in data]
+print("Squares List:", squares)
+
+unique_evens = {x for x in data if x % 2 == 0}
+print("Unique Even Numbers Set:", unique_evens)
+
+cubes_dict = {x: x**3 for x in data}
+print("Cubes Dictionary:", cubes_dict)
