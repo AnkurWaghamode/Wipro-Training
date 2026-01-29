7af34f33e96bf07d4be526e50e78c90298dad356
AnkurWaghamode <168619721+AnkurWaghamode@users.noreply.github.com>
2026-01-29 18:15:28 +0530

    Create conftest.py

---
+++ Day 10 28-01-2026/LAB Assessment/Parameterization, CLI Options, Skips & Expected Failures/conftest.py
@@ -0,0 +1,13 @@
+def pytest_addoption(parser):
+    parser.addoption(
+        "--env",
+        action="store",
+        default="dev",
+        help="Environment to run tests against (dev/stage/prod)"
+    )
+
+import pytest
+
+@pytest.fixture
+def env(request):
+    return request.config.getoption("--env")
